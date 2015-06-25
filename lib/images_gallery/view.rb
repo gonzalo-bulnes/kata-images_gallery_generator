1714300afe2a9250c7d1b369f6d98cee55db5361
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-06-25 01:54:01 -0300

    Refactor extract view helper #link_to

--- lib/images_gallery/view.rb
+++ lib/images_gallery/view.rb
@@ -24,5 +24,17 @@ module ImagesGallery
       template = 'lib/images_gallery/templates/_navigation.html.erb'
       ERB.new(File.new(template).read).result(binding)
     end
+
+    def link_to(depth, make, model=nil)
+      ('../' * depth) + file_identifier(make, model) + '.html'
+    end
+
+    def file_identifier(make, model=nil)
+      if model.nil?
+        "#{make.downcase.gsub(/\W/, '_').gsub(/_+/, '_').gsub(/_\Z/, '')}"
+      else
+        "#{make.downcase.gsub(/\W/, '_').gsub(/_+/, '_').gsub(/_\Z/, '')}/#{model.downcase.gsub(/\W/, '_').gsub(/_+/, '_').gsub(/_\Z/, '')}"
+      end
+    end
   end
 end
--- lib/images_gallery/views/make.rb
+++ lib/images_gallery/views/make.rb
@@ -6,15 +6,16 @@ module ImagesGallery
   module Views
     class Make < ImagesGallery::View
 
-      attr_reader :links, :make, :sample_images, :title
-      private :links, :make, :sample_images, :title
+      attr_reader :depth, :links, :make, :sample_images, :title
+      private :depth, :links, :make, :sample_images, :title
 
       def initialize(images)
         super()
 
+        @depth = 0
         @make = images.first.make
-        @links = [{ name: "Browse all the images", href: "index.html" }]
-        @links += images.models.map{ |model| { name: model, href: "#{make.downcase.gsub(/\W/, '_').gsub(/_+/, '_').gsub(/_\Z/, '')}/#{model.downcase.gsub(/\W/, '_').gsub(/_+/, '_').gsub(/_\Z/, '')}.html" } }
+        @links = [{ name: "Browse all the images", href: link_to(depth, 'index') }]
+        @links += images.models.map{ |model| { name: model, href: link_to(depth, make, model) } }
         @sample_images = images.first(10)
         @title = "Images by #{make}"
       end
