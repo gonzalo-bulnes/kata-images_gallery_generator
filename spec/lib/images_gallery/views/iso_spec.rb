e39954648d106faa09d08c4e5f71583ef650c0e0
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-08-21 00:20:44 -0300

    Fixup add Views::ISO failing specs

--- spec/lib/images_gallery/views/iso_spec.rb
+++ spec/lib/images_gallery/views/iso_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 module ImagesGallery
-describe 'Views::Iso' do
+describe 'Views::ISO' do
 
   let(:data) { images_collection }
-  let(:view) { Views::Iso.new(data) }
+  let(:view) { Views::ISO.new(data) }
 
   it_behaves_like 'a view'
 
