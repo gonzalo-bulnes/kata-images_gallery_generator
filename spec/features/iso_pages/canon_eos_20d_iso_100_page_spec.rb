fbd903b39fa8ec97dfbcd6cb72ee096335715334
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-08-21 00:29:31 -0300

    Minor fix number of thumbnails in feature specs sample

--- spec/features/iso_pages/canon_eos_20d_iso_100_page_spec.rb
+++ spec/features/iso_pages/canon_eos_20d_iso_100_page_spec.rb
@@ -9,6 +9,6 @@ describe 'The Canon EOS 20D @ ISO 100 page', type: :feature do
     visit '/canon/canon_eos_20d/100.html'
   end
 
-  it_behaves_like('an images gallery', 3, header_selector, navigation_selector, title_selector)
+  it_behaves_like('an images gallery', 1, header_selector, navigation_selector, title_selector)
   it_behaves_like('an ISO page', 'canon', 'canon_eos_20d', navigation_selector)
 end
--- spec/features/iso_pages/lux_d_3_iso_200_page_spec.rb
+++ spec/features/iso_pages/lux_d_3_iso_200_page_spec.rb
@@ -9,6 +9,6 @@ describe 'The LUX-D 3 @ ISO 200 page', type: :feature do
     visit '/leica/d_lux_3/200.html'
   end
 
-  it_behaves_like('an images gallery', 3, header_selector, navigation_selector, title_selector)
+  it_behaves_like('an images gallery', 1, header_selector, navigation_selector, title_selector)
   it_behaves_like('an ISO page', 'leica', 'd_lux_3', navigation_selector)
 end
