0aa3ff607d6f0a5fa9c959ad5665d2d50a4ce3b8
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2015-08-20 23:21:37 -0300

    Add views #link_to helper specs

--- spec/support/spec_for_view_inerface.rb
+++ spec/support/spec_for_view_inerface.rb
@@ -1,5 +1,9 @@
 RSpec.shared_examples 'a view' do
 
+  it 'responds to :link_to' do
+    expect(view).to respond_to :link_to
+  end
+
   it 'responds to :render' do
     expect(view).to respond_to :render
   end
@@ -15,5 +19,24 @@ RSpec.shared_examples 'a view' do
   context 'when built with no data' do
 
     it 'exits gracefully (?)'
+  end
+
+  describe '#link_to' do
+
+    let(:depth) { 1 }
+
+    context 'when provided a depth and a make' do
+
+      it 'generates a make page URL' do
+        expect(view.link_to(depth, 'make')).to eq '../make.html'
+      end
+    end
+
+    context 'when provided a depth, a make and a model' do
+
+      it 'generates a model page URL' do
+        expect(view.link_to(depth, 'make', 'model')).to eq '../make/model.html'
+      end
+    end
   end
 end
